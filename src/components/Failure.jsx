7200cd3f4fc0b62a7b84f03bc648e196ce564420
Toan Ngo <tdngo22@gmail.com>
2024-01-30 18:16:44 +0700

    edited meta description

--- src/components/Failure.jsx
+++ src/components/Failure.jsx
@@ -5,16 +5,6 @@ import { TransitionGroup, CSSTransition } from "react-transition-group";
 import WriteButton from "./WriteButton";
 import { withAppContext } from "./AppContext";
 
-const TweetButton = ({ words }) => {
-  const href = `https://twitter.com/intent/tweet?text=I+wrote+${words}+words+using+The+Most+Dangerous+Writing+App+-+until+it+deleted+everything+.+%23MDWA&url=https%3A%2F%2Fmaebert.github.io%2Fthemostdangerouswritingapp%2F`;
-  const label = `I wrote ${words} words using The Most Dangerous Writing App - until it deleted everything.`;
-  return (
-    <a className="tweet" href={href}>
-      {label}
-    </a>
-  );
-};
-
 const FacebookButton = ({ words }) => {
 const href = `https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwrite.together.vn`;
 const label = `Chia sẻ trên Facebook về việc tôi đã viết được ${words} từ trên Write on Fire cho đến khi nó xoá hết công sức của tôi!`;
